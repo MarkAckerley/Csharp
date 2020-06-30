cf2264f3ec19f7568c685828c833210006c03a08
MarkAckerley <41670424+MarkAckerley@users.noreply.github.com>
2020-06-30 10:20:51 +0100

    Add files via upload

---
+++ InfoPanel/InfoPanel/App.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.Attributes;
+using UIFramework;
+using adWin = Autodesk.Windows;
+using Autodesk.Internal.Windows;
+using System.Windows.Media;
+using System.IO;
+
+namespace InfoPanel
+{
+    
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class App : IExternalApplication
+    {
+        // class instance 
+        public static App ThisApp;
+
+        public static NewForm myForm
+        {
+            get;
+            set;
+        }
+
+        public static DockablePaneId paneId
+        {
+            get
+            {
+                return new DockablePaneId(new Guid("9d7ed357-534f-4d87-afc4-8e784e3b116e"));
+            }
+          
+        }
+
+        public Result OnStartup(UIControlledApplication a)
+        {
+            //Register dockable pane
+            NewForm Form = new NewForm();
+           // DockablePaneId paneId = new DockablePaneId(new Guid("9d7ed357-534f-4d87-afc4-8e784e3b119e"));
+
+            myForm = Form;
+
+            ThisApp = this;
+
+            a.RegisterDockablePane(paneId, "Info", Form as IDockablePaneProvider);
+
+            // Method to add Tab and Panel 
+            RibbonPanel panel = RibbonPanel(a);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // BUTTON FOR THE DOCKABLE WPF
+            if (panel.AddItem(
+                new PushButtonData("InfoPanel", "InfoPanel", thisAssemblyPath,
+                    "InfoPanel.EntryCommand")) is PushButton button)
+            {
+                // defines the tooltip displayed when the button is hovered over in Revit's ribbon
+                button.ToolTip = "Turn Firm Info On/Off";
+                // defines the icon for the button in Revit's ribbon - note the string formatting
+                Uri uriImage = new Uri("pack://application:,,,/InfoPanel;component/Resources/code-small.png");
+                BitmapImage largeImage = new BitmapImage(uriImage);
+                button.LargeImage = largeImage;
+            }
+
+
+            //adWin.RibbonControl ribbon = adWin.ComponentManager.Ribbon;
+            //TabTheme.PanelBackgroundProperty = new SolidColorBrush(Colors.HotPink);
+
+
+            //foreach (adWin.RibbonTab tab in ribbon.Tabs)
+            //{
+            //    foreach (adWin.RibbonPanel panelT in tab.Panels)
+            //    {
+            //        panelT.CustomPanelTitleBarBackground
+            //          = new SolidColorBrush(Colors.HotPink);
+            //       panelT.CustomPanelBackground = new SolidColorBrush(Colors.HotPink);
+            //    }
+            //}
+
+            //myForm.Background = new SolidColorBrush(Colors.HotPink);
+            // myForm.IsVisible
+            //myForm.Foreground = new SolidColorBrush(Colors.HotPink);
+            //int op = 50;
+            //myForm.Opacity = op;
+
+
+            // listeners/watchers for external events (if you choose to use them)
+            a.ApplicationClosing += a_ApplicationClosing; //Set Application to Idling
+            a.Idling += a_Idling;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            //Register dockable pane          
+            return Result.Succeeded;
+        }
+        /// <summary>
+        /// This is the method which launches the WPF window, and injects any methods that are
+        /// wrapped by ExternalEventHandlers. This can be done in a number of different ways, and
+        /// implementation will differ based on how the WPF is set up.
+        /// </summary>
+        /// <param name="uiapp">The Revit UIApplication within the add-in will operate.</param>
+        public void ShowForm(ExternalCommandData commandData)
+        {
+            DockablePane myForm = commandData.Application.GetDockablePane(paneId);
+            // If we do not have a dialog yet, create and show it
+            if (myForm != null && myForm == null) return;
+            // The dialog becomes the owner responsible for disposing the objects given to it.
+            //myForm = new NewForm(evP, evPB);
+
+            myForm.Show();
+        }
+
+        /// <summary>
+        /// What to do when the application is idling. (Ideally nothing)
+        /// </summary>
+        void a_Idling(object sender, IdlingEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// What to do when the application is closing.)
+        /// </summary>
+        void a_ApplicationClosing(object sender, ApplicationClosingEventArgs e)
+        {
+        }
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tab = "Dyna-Sco"; // Tab name
+            // Empty ribbon panel 
+            RibbonPanel ribbonPanel = null;
+            // Try to create ribbon tab. 
+            try
+            {
+                a.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Util.HandleError(ex);
+            }
+
+            // Try to create ribbon panel.
+            try
+            {
+                RibbonPanel panel = a.CreateRibbonPanel(tab, "Visibility");
+            }
+            catch (Exception ex)
+            {
+                Util.HandleError(ex);
+            }
+
+            // Search existing tab for your panel.
+            List<RibbonPanel> panels = a.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "Visibility"))
+            {
+                ribbonPanel = p;
+            }
+
+            //return panel 
+            return ribbonPanel;
+        }
+    }
+}
---
+++ InfoPanel/InfoPanel/Form.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Autodesk.Revit.UI;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InfoPanel
+{
+    public partial class NewForm : Page, IDockablePaneProvider
+    {
+
+        public void SetupDockablePane(DockablePaneProviderData data)
+        {
+            data.FrameworkElement = this as FrameworkElement;
+            data.InitialState = new DockablePaneState();
+            data.InitialState.MinimumWidth = 300;
+            data.VisibleByDefault = true;
+            data.InitialState.DockPosition = DockPosition.Tabbed;
+            data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+        }
+
+        public NewForm()
+        {
+            InitializeComponent();
+
+        }
+
+
+        /// Tab 1 These are the web pages which are not PDFs
+
+        private void Web_Click(object sender, RoutedEventArgs e)
+        {
+            var bItem = (Button)sender;
+            WebP.Navigate(bItem.Tag.ToString());
+        }
+
+        /// This is where we set the forward & back buttons for the web pages
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((WebP != null) && (WebP.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            WebP.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((WebP != null) && (WebP.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            WebP.GoForward();
+        }
+
+        //Tab2
+        private void InfoMenu_Click(object sender, RoutedEventArgs e)
+        {
+            var mItem = (MenuItem)sender;
+            Firm_Info.Navigate(mItem.Tag.ToString());
+        }
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var bItem = (Button)sender;
+            Firm_Info.Navigate(bItem.Tag.ToString());
+        }
+
+
+
+        // Tab 3
+        /// These are all PDFs, if we load PDFs and webpages in the same tabs we lose the scroll point of the PDF
+
+        private void StandardsMenu_Click(object sender, RoutedEventArgs e)
+        {
+            var mItem = (MenuItem)sender;
+            Standards.Navigate(mItem.Tag.ToString());
+        }
+
+        private void StandardsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var bItem = (Button)sender;
+            Standards.Navigate(bItem.Tag.ToString());
+        }
+
+
+        //These catch nasty popups
+
+        void WebBrowser_Navigated(   object sender,   NavigationEventArgs e)
+        {
+            HideJsScriptErrors((WebBrowser)sender);
+        }
+
+        public void HideJsScriptErrors(WebBrowser WebP)
+        {
+            // IWebBrowser2 interface
+            // Exposes methods that are implemented by the 
+            // WebBrowser control
+            // Searches for the specified field, using the 
+            // specified binding constraints.
+
+            FieldInfo fld = typeof(WebBrowser).GetField(
+              "_axIWebBrowser2",
+              BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (null != fld)
+            {
+                object obj = fld.GetValue(WebP);
+                if (null != obj)
+                {
+
+                    obj.GetType().InvokeMember("Silent",
+                      BindingFlags.SetProperty, null, obj,
+                      new object[] { true });
+                }
+            }
+
+        }
+
+    }
+
+}
